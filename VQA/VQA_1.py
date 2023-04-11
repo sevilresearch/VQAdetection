5c286e03fb90541a381f1224e74eb0f841178686
sevilresearch <122336432+sevilresearch@users.noreply.github.com>
2023-04-10 21:53:07 -0500

    Create VQA_1.py

---
+++ VQA/VQA_1.py
@@ -0,0 +1,374 @@
+import cv2 as cv
+import numpy as np
+from time import time
+import pyautogui
+import imutils
+from vision import Vision
+#background = cv.imread('Scenario.JPG', cv.IMREAD_UNCHANGED)
+background = cv.imread('Scenario.jpg', cv.IMREAD_UNCHANGED)
+#Object = cv.imread('Paper.JPG', cv.IMREAD_UNCHANGED)
+Object = cv.imread('Paper.jpg',cv.IMREAD_UNCHANGED)
+object_w = Object.shape[1]
+object_h = Object.shape[0]
+result = cv.matchTemplate(background, Object, cv.TM_CCOEFF_NORMED)
+print(result)
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+threshold = 0.6
+location = np.where(result >= threshold)
+print(location)
+location = list(zip(*location[::-1]))
+
+print(location)
+print(max_val)
+rectangle = []
+for loc in location:
+rect = [int(loc[0]), int(loc[1]), object_w, object_h]
+rectangle.append(rect)
+rectangle.append(rect)
+rectangle, weights = cv.groupRectangles(rectangle, 1, 0.85)
+print(rectangle)
+if len(rectangle):
+print('Found object!')
+line_color = (0,255,0)
+thickness = 3
+line_type = cv.LINE_4
+for (x,y,w,h) in rectangle:
+top_left = (x, y)
+bottom_right = (x + w, y + h)
+cv.rectangle(background,top_left,bottom_right, line_color, thickness, lin
+e_type)
+cv.putText(background, 'ToiletPaper', top_left, cv.FONT_HERSHEY_SIMPLEX,
+0.7, (255,255,0), 1)
+cv.imshow('Results', background)
+cv.waitKey()
+else:
+print('Not Found!')
+import cv2 as cv
+import numpy as np
+from time import time
+import pyautogui
+import imutils
+from vision import Vision
+cap = cv.VideoCapture('Bldg79A.MP4')
+
+cascade_final = cv.CascadeClassifier('cascade2/cascade.xml')
+vision_limestone = Vision(None)
+def rescale_frame(frame, percent=75):
+width = int(frame.shape[1] * percent/ 100)
+height = int(frame.shape[0] * percent/ 100)
+dim = (width, height)
+return cv.resize(frame, dim, interpolation =cv.INTER_AREA)
+if (cap.isOpened()== False):
+print("Error opening video stream or file")
+loop_time = time()
+currentframe = 0
+frameTime = 0
+while(cap.isOpened()):
+ret, frame = cap.read()
+if ret == True:
+frame30 = rescale_frame(frame, percent=30)
+rectangles = cascade_final.detectMultiScale(frame30, 1.1, 2,0,(10, 10),(100,1
+00))
+detection_image = vision_limestone.draw_rectangles(frame30, rectangles)
+cv.imshow('Frame',frame30)
+key = cv.waitKey(1)
+if key == ord('q'):
+cv.destroyAllWindows()
+break
+elif key == ord('p'):
+name = './Bldg79 38-48/frame' + str(currentframe) + '.jpg'
+print ('Creating...' + name)
+# writing the extracted images
+cv.imwrite(name, frame30)
+# increasing counter so that it will
+# show how many frames are created
+currentframe = currentframe + 1
+#cv.imwrite('positive1/{}.jpg', format(loop_time), frame30)
+elif key == ord('n'):
+name = './79Neg/frame' + str(currentframe) + '.jpg'
+print ('Creating...' + name)
+# writing the extracted images
+cv.imwrite(name, frame30)
+# increasing counter so that it will
+# show how many frames are created
+currentframe = currentframe + 1
+#cv.imwrite('negative1/{}.jpg', format(loop_time), frame30)
+# Break the loop
+else:
+break
+cap.release()
+cv.destroyAllWindows()
+import cv2 as cv
+import numpy as np
+from hsvfilter import HsvFilter
+from edgefilter import EdgeFilter
+class Vision:
+TRACKBAR_WINDOW = "Trackbars"
+needle_img = None
+needle_w = 0
+needle_h = 0
+method = None
+def __init__(self, needle_img_path, method=cv.TM_CCOEFF_NORMED):
+if needle_img_path:
+self.needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
+self.needle_w = self.needle_img.shape[1]
+self.needle_h = self.needle_img.shape[0]
+self.method = method
+
+def find(self, haystack_img, threshold=0.5, max_results=10):
+result = cv.matchTemplate(haystack_img, self.needle_img, self.method)
+locations = np.where(result >= threshold)
+locations = list(zip(*locations[::-1]))
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+# The max location will contain the upper left corner pixel position for
+the area
+# that most closely matches our needle image. The max value gives an indi
+cation
+# of how similar that find is to the original needle, where 1 is perfect
+and -1
+# is exact opposite.
+print('Best match top left position: %s' % str(max_loc))
+print('Best match confidence: %s' % max_val)
+if not locations:
+return np.array([], dtype=np.int32).reshape(0, 4)
+rectangles = []
+for loc in locations:
+rect = [int(loc[0]), int(loc[1]), self.needle_w, self.needle_h]
+rectangles.append(rect)
+rectangles.append(rect)
+rectangles, weights = cv.groupRectangles(rectangles, groupThreshold=1, ep
+s=0.5)
+if len(rectangles) > max_results:
+print('Warning: too many results, raise the threshold.')
+rectangles = rectangles[:max_results]
+return rectangles
+def get_click_points(self, rectangles):
+points = []
+for (x, y, w, h) in rectangles:
+center_x = x + int(w/2)
+center_y = y + int(h/2)
+
+points.append((center_x, center_y))
+return points
+def draw_rectangles(self, haystack_img, rectangles):
+line_color = (0, 255, 0)
+line_type = cv.LINE_4
+for (x, y, w, h) in rectangles:
+top_left = (x, y)
+bottom_right = (x + w, y + h)
+cv.rectangle(haystack_img, top_left, bottom_right, line_color, lineTy
+pe=line_type)
+cv.putText(haystack_img, 'DAMAGE', top_left, cv.FONT_HERSHEY_SIMPLEX,
+0.7, (0,0,255), 1)
+cv.putText(haystack_img, 'Less than 40%', bottom_right, cv.FONT_HERSH
+EY_SIMPLEX, 0.7, (0,255,255), 1)
+return haystack_img
+def draw_crosshairs(self, haystack_img, points):
+marker_color = (255, 0, 255)
+marker_type = cv.MARKER_CROSS
+for (center_x, center_y) in points:
+cv.drawMarker(haystack_img, (center_x, center_y), marker_color, marke
+r_type)
+return haystack_img
+def init_control_gui(self):
+cv.namedWindow(self.TRACKBAR_WINDOW, cv.WINDOW_NORMAL)
+cv.resizeWindow(self.TRACKBAR_WINDOW, 350, 700)
+def nothing(position):
+pass
+
+cv.createTrackbar('HMin', self.TRACKBAR_WINDOW, 0, 179, nothing)
+cv.createTrackbar('SMin', self.TRACKBAR_WINDOW, 0, 255, nothing)
+cv.createTrackbar('VMin', self.TRACKBAR_WINDOW, 0, 255, nothing)
+cv.createTrackbar('HMax', self.TRACKBAR_WINDOW, 0, 179, nothing)
+cv.createTrackbar('SMax', self.TRACKBAR_WINDOW, 0, 255, nothing)
+cv.createTrackbar('VMax', self.TRACKBAR_WINDOW, 0, 255, nothing)
+cv.setTrackbarPos('HMax', self.TRACKBAR_WINDOW, 179)
+cv.setTrackbarPos('SMax', self.TRACKBAR_WINDOW, 255)
+cv.setTrackbarPos('VMax', self.TRACKBAR_WINDOW, 255)
+cv.createTrackbar('SAdd', self.TRACKBAR_WINDOW, 0, 255, nothing)
+cv.createTrackbar('SSub', self.TRACKBAR_WINDOW, 0, 255, nothing)
+cv.createTrackbar('VAdd', self.TRACKBAR_WINDOW, 0, 255, nothing)
+cv.createTrackbar('VSub', self.TRACKBAR_WINDOW, 0, 255, nothing)
+cv.createTrackbar('KernelSize', self.TRACKBAR_WINDOW, 1, 30, nothing)
+cv.createTrackbar('ErodeIter', self.TRACKBAR_WINDOW, 1, 5, nothing)
+cv.createTrackbar('DilateIter', self.TRACKBAR_WINDOW, 1, 5, nothing)
+cv.createTrackbar('Canny1', self.TRACKBAR_WINDOW, 0, 200, nothing)
+cv.createTrackbar('Canny2', self.TRACKBAR_WINDOW, 0, 500, nothing)
+cv.setTrackbarPos('KernelSize', self.TRACKBAR_WINDOW, 5)
+cv.setTrackbarPos('Canny1', self.TRACKBAR_WINDOW, 100)
+cv.setTrackbarPos('Canny2', self.TRACKBAR_WINDOW, 200)
+def get_hsv_filter_from_controls(self):
+hsv_filter = HsvFilter()
+hsv_filter.hMin = cv.getTrackbarPos('HMin', self.TRACKBAR_WINDOW)
+hsv_filter.sMin = cv.getTrackbarPos('SMin', self.TRACKBAR_WINDOW)
+hsv_filter.vMin = cv.getTrackbarPos('VMin', self.TRACKBAR_WINDOW)
+hsv_filter.hMax = cv.getTrackbarPos('HMax', self.TRACKBAR_WINDOW)
+hsv_filter.sMax = cv.getTrackbarPos('SMax', self.TRACKBAR_WINDOW)
+hsv_filter.vMax = cv.getTrackbarPos('VMax', self.TRACKBAR_WINDOW)
+hsv_filter.sAdd = cv.getTrackbarPos('SAdd', self.TRACKBAR_WINDOW)
+hsv_filter.sSub = cv.getTrackbarPos('SSub', self.TRACKBAR_WINDOW)
+hsv_filter.vAdd = cv.getTrackbarPos('VAdd', self.TRACKBAR_WINDOW)
+hsv_filter.vSub = cv.getTrackbarPos('VSub', self.TRACKBAR_WINDOW)
+return hsv_filter
+def get_edge_filter_from_controls(self):
+edge_filter = EdgeFilter()
+
+edge_filter.kernelSize = cv.getTrackbarPos('KernelSize', self.TRACKBAR_WI
+NDOW)
+edge_filter.erodeIter = cv.getTrackbarPos('ErodeIter', self.TRACKBAR_WIND
+OW)
+edge_filter.dilateIter = cv.getTrackbarPos('DilateIter', self.TRACKBAR_WI
+NDOW)
+edge_filter.canny1 = cv.getTrackbarPos('Canny1', self.TRACKBAR_WINDOW)
+edge_filter.canny2 = cv.getTrackbarPos('Canny2', self.TRACKBAR_WINDOW)
+return edge_filter
+def apply_hsv_filter(self, original_image, hsv_filter=None):
+hsv = cv.cvtColor(original_image, cv.COLOR_BGR2HSV)
+if not hsv_filter:
+hsv_filter = self.get_hsv_filter_from_controls()
+h, s, v = cv.split(hsv)
+s = self.shift_channel(s, hsv_filter.sAdd)
+s = self.shift_channel(s, -hsv_filter.sSub)
+v = self.shift_channel(v, hsv_filter.vAdd)
+v = self.shift_channel(v, -hsv_filter.vSub)
+hsv = cv.merge([h, s, v])
+lower = np.array([hsv_filter.hMin, hsv_filter.sMin, hsv_filter.vMin])
+upper = np.array([hsv_filter.hMax, hsv_filter.sMax, hsv_filter.vMax])
+mask = cv.inRange(hsv, lower, upper)
+result = cv.bitwise_and(hsv, hsv, mask=mask)
+img = cv.cvtColor(result, cv.COLOR_HSV2BGR)
+return img
+def apply_edge_filter(self, original_image, edge_filter=None):
+if not edge_filter:
+edge_filter = self.get_edge_filter_from_controls()
+kernel = np.ones((edge_filter.kernelSize, edge_filter.kernelSize), np.uin
+t8)
+eroded_image = cv.erode(original_image, kernel, iterations=edge_filter.er
+odeIter)
+dilated_image = cv.dilate(eroded_image, kernel, iterations=edge_filter.di
+lateIter)
+
+result = cv.Canny(dilated_image, edge_filter.canny1, edge_filter.canny2)
+img = cv.cvtColor(result, cv.COLOR_GRAY2BGR)
+return img
+def shift_channel(self, c, amount):
+if amount > 0:
+lim = 255 - amount
+c[c >= lim] = 255
+c[c < lim] += amount
+elif amount < 0:
+amount = -amount
+lim = amount
+c[c <= lim] = 0
+c[c > lim] -= amount
+return c
+def match_keypoints(self, original_image, patch_size=32):
+min_match_count = 5
+orb = cv.ORB_create(edgeThreshold=0, patchSize=patch_size)
+keypoints_needle, descriptors_needle = orb.detectAndCompute(self.needle_i
+mg, None)
+orb2 = cv.ORB_create(edgeThreshold=0, patchSize=patch_size, nfeatures=200
+0)
+keypoints_haystack, descriptors_haystack = orb2.detectAndCompute(original
+_image, None)
+FLANN_INDEX_LSH = 6
+index_params = dict(algorithm=FLANN_INDEX_LSH,
+table_number=6,
+key_size=12,
+multi_probe_level=1)
+search_params = dict(checks=50)
+try:
+flann = cv.FlannBasedMatcher(index_params, search_params)
+matches = flann.knnMatch(descriptors_needle, descriptors_haystack, k=
+2)
+except cv.error:
+return None, None, [], [], None
+
+good = []
+points = []
+for pair in matches:
+if len(pair) == 2:
+if pair[0].distance < 0.7*pair[1].distance:
+good.append(pair[0])
+if len(good) > min_match_count:
+print('match %03d, kp %03d' % (len(good), len(keypoints_needle)))
+for match in good:
+points.append(keypoints_haystack[match.trainIdx].pt)
+return keypoints_needle, keypoints_haystack, good, points
+def centeroid(self, point_list):
+point_list = np.asarray(point_list, dtype=np.int32)
+length = point_list.shape[0]
+sum_x = np.sum(point_list[:, 0])
+sum_y = np.sum(point_list[:, 1])
+return [np.floor_divide(sum_x, length), np.floor_divide(sum_y, length)]
+import cv2 as cv
+import numpy as np
+import os
+import pyautogui
+from time import time
+#from PIL import ImageGrab
+import win32gui
+import win32ui, win32con
+#import WindowCapture
+class WindowCapture:
+w = 0
+h = 0
+hwnd = None
+cropped_x = 0
+cropped_y = 0
+offset_x = 0
+offset_y = 0
+def __init__(self, window_name=None):
+if window_name is None:
+self.hwnd = win32gui.GetDesktopWindow()
+
+else:
+self.hwnd = win32gui.FindWindow(None, window_name)
+if not self.hwnd:
+raise Exception("Window not found: {}", format(window_name))
+window_rect = win32gui.GetWindowRect(self.hwnd)
+self.w = window_rect[2] - window_rect[0]
+self.h = window_rect[3] - window_rect[1]
+border_pixels = 8
+titlebar_pixels = 30
+self.w = self.w - (border_pixels * 2)
+self.h = self.h - titlebar_pixels - border_pixels
+self.cropped_x = border_pixels
+self.cropped_y = titlebar_pixels
+self.offset_x = window_rect[0] + self.cropped_x
+self.offset_y = window_rect[1] + self.cropped_y
+def get_screenshot(self):
+w = 1920
+h = 1080
+#hwnd = win32gui.FindWindow(None, 'Wheely 2 - Play Wheely 2 on Crazy Game
+s - Google Chrome')
+#hwnd = None
+wDC = win32gui.GetWindowDC(self.hwnd)
+dcObj=win32ui.CreateDCFromHandle(wDC)
+cDC=dcObj.CreateCompatibleDC()
+dataBitMap = win32ui.CreateBitmap()
+dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
+cDC.SelectObject(dataBitMap)
+cDC.BitBlt((0,0),(self.w, self.h) , dcObj, (self.cropped_x,self.cropped_y
+), win32con.SRCCOPY)
+#dataBitMap.SaveBitmapFile(cDC, 'debug.bmp')
+signedIntsArray = dataBitMap.GetBitmapBits(True)
+img = np.frombuffer(signedIntsArray, dtype='uint8')
+img.shape = (self.h, self.w, 4)
+# Free Resources
+dcObj.DeleteDC()
+cDC.DeleteDC()
+win32gui.ReleaseDC(self.hwnd, wDC)
+
+win32gui.DeleteObject(dataBitMap.GetHandle())
+img = img[...,:3]
+img = np.ascontiguousarray(img)
+return img
+def list_windows_names():
+def winEnumHandler( hwnd, ctx ):
+if win32gui.IsWindowVisible( hwnd ):
+print (hex(hwnd), win32gui.GetWindowText( hwnd ))
+win32gui.EnumWindows( winEnumHandler, None )
+#list_windows_names()
+def get_screen_position(self, pos):
+return (pos[0] + self.offset_x, pos[1] + self.offset_y)
